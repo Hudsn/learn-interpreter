c366f25ca277cb40d92ba9696cb60169c28dfc2a
Hudsn <70234973+Hudsn@users.noreply.github.com>
2025-06-30 02:06:32 -0700

    ch1 done

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+
+	"github.com/hudsn/learn-interpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
+}
